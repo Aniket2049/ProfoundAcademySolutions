5bde5553d8557da11e9e5eb00a46678f1e21e615
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2025-05-19 00:28:29 +0530

    add 03 Divisibility by 11

---
+++ src/_01_implementation/_03_DivisibilityBy11.java
@@ -0,0 +1,63 @@
+package _01_implementation;
+
+import java.util.Scanner;
+
+//https://profound.academy/algorithms-data-structures/divisibility-by-11-JJsIT210xYYDgPXMZlKZ
+
+/*
+Divisibility by 11
+Did you know that there is an easy way to check if a number is divisible by 11?
+For large numbers, we can take all the digits in odd positions and sum them 
+together, after that take all the digits in even positions and sum those 
+together, and subtract those two sums from each other. If the resulting number 
+is divisible by 11, then the whole number is divisible by 11.
+
+Input
+The input contains a very large positive integer that can have up to 106 
+(million) digits.
+Output
+The program should print Yes if the given number is divisible by 11 and 
+No otherwise.
+
+Examples
++---------------+---------------+
+|Input        	|Output			|
++---------------+---------------+
+|563706			|Yes			|
++---------------+---------------+
+|12345678		|No				|
++---------------+---------------+
+*/
+
+public class _03_DivisibilityBy11 {
+
+	public static void main(String[] args) {
+		String num;
+		Scanner input = new Scanner(System.in);
+		num = input.nextLine();
+		input.close();
+
+		//--------Input Read--------
+
+		int n = num.length();
+		int oddSum = 0;
+		int evenSum = 0;
+		boolean isOdd = true;
+		for (int i = n - 1; i >= 0; i--) {
+			Integer number = Integer.parseInt(String.valueOf(num.charAt(i)));
+			if (isOdd) {
+				oddSum += number;
+			} else {
+				evenSum += number;
+			}
+			isOdd = !isOdd;
+		}
+
+		if (Math.abs(oddSum - evenSum) % 11 == 0)
+			System.out.println("Yes");
+		else
+			System.out.println("No");
+
+	}
+
+}
